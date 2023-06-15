64237842126638a7a7e4c1f4d70164cb9b50e4c5
Lucas Maretti <lucas.maretti@gmail.com>
2023-06-15 16:42:35 -0300

    Fixing Home.py markdown

--- Home.py
+++ Home.py
@@ -5,21 +5,21 @@ st.set_page_config(
     page_icon = "🏠")
 
 
-st.sidebar.markdown('# Faltavam só 4 minutos')
+st.sidebar.markdown('# Faltavam 4 minutos')
 st.sidebar.markdown('## Python web app')
 st.sidebar.markdown('## Aplicações de ciência de dados ao futebol')
 #st.sidebar.image('./pages/2022_FIFA_World_Cup.png', use_column_width=True)
 st.sidebar.image('./pages/footy.jpeg', use_column_width=True)
 st.sidebar.markdown("""---""")
 
-st.write('# Faltavam só 4 minutos web app')
+st.write('# Faltavam 4 minutos web app')
 st.write('## Ciência de dados aplicada a dados de futebol')
 
 st.markdown(""" 
 
 **PT/BR**: O objetivo deste webapp é mostrar na prática aplicações de ciência de dados ao futebol. Para tanto, exploraremos duas das principais etapas de um projeto de ciência de dados: análise exploratória (EDA) e aplicação de machine learning.\\
 \\
-A análise exploratória de dados se dará em cima dos dados abertos referentes à Copa do Mundo de 2022 no Qatar fornecidos pela empresa Statsbomb (seção "🏆 Brasil na Copa do Mundo 2022" no menu ao lado). O nome deste webapp - Faltavam só 4 minutos - refere-se ao fatídico 9 de Dezembro de 2022 quando a seleção brasileira, com o jogo dominado e vencendo por 1x0 a Croácia com gol de Neymar, cedeu empate aos croatas em um lance inexplicável até hoje faltando 4 minutos para o fim da prorrogação.\\
+A análise exploratória de dados se dará em cima dos dados abertos referentes à Copa do Mundo de 2022 no Qatar fornecidos pela empresa Statsbomb (seção "🏆 Brasil na Copa do Mundo 2022" no menu ao lado). O nome deste webapp - Faltavam 4 minutos - refere-se ao fatídico 9 de Dezembro de 2022 quando a seleção brasileira, com o jogo dominado e vencendo por 1x0 a Croácia com gol de Neymar, cedeu empate aos croatas em um lance inexplicável até hoje faltando 4 minutos para o fim da prorrogação.\\
 A aplicação de Machine Learning (seção 📈 Machine Learning no futebol: Expected Goals) é referente a um outro conjunto de dados referentes às principais ligas europeias para a temporada 2017/2018 fornecido pela empresa Wyscout, cujo desenvolvimento fez parte de um projeto de pesquisa do programa de mestrado em ciência de dados do MECAI-USP
 \\
 ________________________
