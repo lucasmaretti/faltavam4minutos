1bc59d2f6acb08aaa9472028eb644ffcd64f4b6e
Lucas Maretti <lucas.maretti@gmail.com>
2023-07-14 19:50:38 -0300

    Text changes to Machine Learning section

--- pages/📈 Machine Learning no futebol: Expected Goals.py
+++ pages/📈 Machine Learning no futebol: Expected Goals.py
@@ -73,7 +73,7 @@ def predict_class(features):
 # Create the Streamlit app
 def main():
     st.write('### Modelo de Expected Goals (xG) usando Random Forest Classifier')
-    st.write('Insira valores para as seguintes features:')
+    st.write('Insira valores para os parâmetros abaixo (dica: chutes mais perto do gol e com maior ângulo ajudam ;)):')
 
     # Create input fields for each feature
     inputs = []
